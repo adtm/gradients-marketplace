ca7adb47260eb9ee43fccae285d9325ed95201e5
Tomas Eglinskas <tomase@wix.com>
2021-04-24 23:05:18 +0300

    feat: connect metamask

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,42 +1,81 @@
-import React from 'react'
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+// @ts-nocheck
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useWeb3React } from '@web3-react/core'
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import detectEthereumProvider from '@metamask/detect-provider'
 
- 
+const MAIN_CHAIN_ID = "1666700000"
+const TESTNET_CHAIN_ID = "1666600000"
+
+const HARMONY_CHAIN_IDS = new Set([TESTNET_CHAIN_ID, MAIN_CHAIN_ID])
+
+
 export default function Example() {
-  const { chainId, error } = useWeb3React()
+  
+  const [error, setError] = useState<string | null>(null);
+  const [account, setAccount] = useState<string | null>(null)
+
+  const connectProvider = async () => {
+    const chainId = await ethereum.request({ method: 'eth_chainId' });
+    handleChainIdChanged(chainId);
+
+    setInterval(async () => {
+      const account = await ethereum.request({ method: 'eth_accounts' })
+      handleAccountsChanged(account)
+    }, 1000)
+
+    window.ethereum.on('chainChanged', handleChainIdChanged);
+  }
+
+  const handleAccountsChanged = (accounts) => {
+    if (accounts.length === 0) {
+      setAccount(null);
+    } else if (accounts[0] !== account) {
+      setAccount(accounts[0]);
+    }
+  }
+
+  const handleChainIdChanged = (chainId) => {
+    const chainIdInDec = BigInt(chainId).toString(10)  
+    if (!HARMONY_CHAIN_IDS.has(chainIdInDec)) {
+      setError("Wrong network")
+    } else { setError(null); }
+  }
+
+  const connect = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      if (provider !== window.ethereum) {
+        return setError("Multiple wallets detected")
+      }
+      connectProvider()
+    } else {
+      setError("MetaMask not installed")
+    }
+  } 
+
+  useEffect(() => {
+    connect();
+  }, [])
+  
+  const buttonText = () => {
+    if (error) return error
+    if (account) return "Connected" 
+    return "Connect Metamask"
+  }
 
   const checkIfCorrectNetwork = () => {
-    console.log(chainId)
-    if (error) {
-      return (
-        <button className="py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-red-500 hover:bg-red-700">
-                        Wrong Network
-                      </button>
-      )
-    }
-
-    if (!chainId) {
-      return (
-        <button className="py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700">
-                      Connect Wallet
-                    </button> 
-      )
-    }
-
-    if (chainId == 1666700000 || chainId == 1666600000) {
-      return <button className="py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">
-      Connected 
-    </button> 
+    const buttonStyles = () => {
+      if (error) return "text-white bg-red-500 hover:bg-red-700"
+      if (account) return "text-white bg-green-500 hover:bg-green-700"
+      return "text-white bg-black hover:bg-gray-700"
     }
 
     return (
-      <button className="py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-red-500 hover:bg-red-700">
-                      Wrong Network
-                    </button>
+      <button onClick={window.ethereum.enable}  className={`py-3 px-6 font-semibold rounded-lg shadow-md ${buttonStyles()}`}>
+        { buttonText() }
+      </button>
     )
   }
 
@@ -50,12 +89,12 @@ export default function Example() {
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
                   <Link to="/">
-                   
+
                     <h1 className="font-bold text-2xl">ichi<span className="text-red-500">.</span></h1>
-                  
+
                   </Link>
                 </div>
-                <div className="hidden sm:flex sm:ml-10 w-full justify-end"> 
+                <div className="hidden sm:flex sm:ml-10 w-full justify-end">
                   <div className="space-x-5">
                     <Link to="/create">
                       <button className=" py-3 px-6 font-semibold rounded-lg shadow-md text-black bg-white hover:bg-gray-100">
@@ -63,8 +102,8 @@ export default function Example() {
                     </button>
 
                     </Link>
-                    { checkIfCorrectNetwork()  } 
-                    
+                    {checkIfCorrectNetwork()}
+
                   </div>
                 </div>
               </div>
@@ -96,11 +135,11 @@ const MobilePanel = () => (
       <Link to="/create">
         <Disclosure.Button className="w-full py-3 px-6 font-semibold text-black bg-white">
           Create
-      </Disclosure.Button> 
-      </Link> 
-      <Disclosure.Button  className="w-full py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700">
+      </Disclosure.Button>
+      </Link>
+      <Disclosure.Button className="w-full py-3 px-6 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700">
         Connect Wallet
-      </Disclosure.Button> 
+      </Disclosure.Button>
     </div>
   </Disclosure.Panel>
 )
