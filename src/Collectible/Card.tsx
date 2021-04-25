372382c81a7a87b3d3364f8de6fe56dce7174abe
Tomas Eglinskas <tomase@wix.com>
2021-04-25 15:13:37 +0300

    fix: card mobile width/height default image

--- src/Collectible/Card.tsx
+++ src/Collectible/Card.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Card = ({ collectible }: any) => {
   return (
     <div className="sm:w-56 m-3" >
-      <div className="rounded-md rounded-b-none sm:h-56 bg-white object-cover bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"  />
+      <div className="rounded-md rounded-b-none h-96 w-96 sm:w-56 sm:h-56 bg-white object-cover bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500"  />
       <div className="shadow-xl rounded-t-none rounded-md">
         <div className="px-4 py-4">
           <span className="text-xs text-gray-500 font-bold">{collectible.quantity.number} of {collectible.quantity.from}</span>
