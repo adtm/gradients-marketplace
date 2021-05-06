4bcb6e42e0edb4276bc6c372ef7d658af4949aaf
Tomas Eglinskas <tomase@wix.com>
2021-05-06 10:42:52 +0300

    feat: more test cases and todos for buy

--- test/GradientMarketplace.spec.js
+++ test/GradientMarketplace.spec.js
@@ -5,6 +5,9 @@ const {
   expectEvent,
   expectRevert
 } = require('@openzeppelin/test-helpers');
+const faker = require("faker")
+// @NOTE: cleanups
+// @NOTE: random values
 
 contract("Gradient marketplace", accounts => {
 
@@ -13,10 +16,16 @@ contract("Gradient marketplace", accounts => {
 
   const tokenId = new BN(1);
   const price = new BN(20);
+  
+  let leftColor;
+  let rightColor;
 
   beforeEach(async () => {
     tokenInstance = await GradientToken.deployed();
     markeplaceInstance = await GradientMarketplace.deployed(tokenInstance.address);
+
+    leftColor = faker.commerce.color();
+    rightColor = faker.commerce.color();
   })
 
   it("should receive nft address on constructor", async () => {
@@ -28,7 +37,7 @@ contract("Gradient marketplace", accounts => {
   describe("sellGradient", () => {
 
     it("should put a gradient for sale by owner", async () => {
-      await tokenInstance.createGradient("A", "A", { from: accounts[0] });
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
       await markeplaceInstance.sellGradient(tokenId, price, {
         from: accounts[0]
       });
@@ -41,10 +50,10 @@ contract("Gradient marketplace", accounts => {
     })
 
     it("shouldn't allow to sell gradient to not an owner", async () => {
-      await tokenInstance.createGradient("A", "A", { from: accounts[0] });
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
 
       await expectRevert(
-        markeplaceInstance.sellGradient(new BN(1), new BN(20), {
+        markeplaceInstance.sellGradient(new BN(2), new BN(20), {
           from: accounts[1]
         }),
         "Gradient doesn't belong to you"
@@ -52,10 +61,10 @@ contract("Gradient marketplace", accounts => {
     });
 
     it("shouldn't allow to sell gradient for 0 price", async () => {
-      await tokenInstance.createGradient("A", "A", { from: accounts[0] });
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
 
       await expectRevert(
-        markeplaceInstance.sellGradient(new BN(1), new BN(0), {
+        markeplaceInstance.sellGradient(new BN(3), new BN(0), {
           from: accounts[0]
         }),
         "Sell price cannot be negative or zero"
@@ -66,16 +75,16 @@ contract("Gradient marketplace", accounts => {
   describe("cancelSellGradient", () => {
 
     it("shouldn't allow to cancel a gradient sale to not an owner", async () => {
-      await tokenInstance.createGradient("A", "A", { from: accounts[0] });
-      await markeplaceInstance.sellGradient(tokenId, price, {
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
+      await markeplaceInstance.sellGradient(new BN(4), price, {
         from: accounts[0]
       });
 
-      await  markeplaceInstance.cancelSellGradient(new BN(1), {
+      await markeplaceInstance.cancelSellGradient(new BN(4), {
         from: accounts[0]
       });
 
-      const result = await markeplaceInstance.sellTransactionByTokenId.call(1);
+      const result = await markeplaceInstance.sellTransactionByTokenId.call(4);
       assert.deepEqual(result[0].toString(), '0');
       assert.deepEqual(result[1].toString(), '0x0000000000000000000000000000000000000000');
       assert.deepEqual(result[2], false);
@@ -83,18 +92,45 @@ contract("Gradient marketplace", accounts => {
     });
 
     it("shouldn't allow to cancel a gradient sale to not an owner", async () => {
-      await tokenInstance.createGradient("A", "A", { from: accounts[0] });
-      await markeplaceInstance.sellGradient(tokenId, price, {
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
+      await markeplaceInstance.sellGradient(new BN(5), price, {
         from: accounts[0]
       });
 
       await expectRevert(
-        markeplaceInstance.cancelSellGradient(new BN(1), {
+        markeplaceInstance.cancelSellGradient(new BN(5), {
           from: accounts[1]
         }),
         "Gradient doesn't belong to you"
       );
     });
   });
-  
+
+  describe("buyGradient", () => {
+
+    it("shouldn't allow to buy a gradient, which is not for sale", async () => {
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
+
+      await expectRevert(
+        markeplaceInstance.buyGradient(new BN(6), {
+          from: accounts[1]
+        }),
+        "Gradient is not for sale"
+      );  
+    });
+
+    it("shouldn't allow to buy an own gradient for the owner", async () => {
+      await tokenInstance.createGradient(leftColor, rightColor, { from: accounts[0] });
+      await markeplaceInstance.sellGradient(new BN(7), price, {
+        from: accounts[0]
+      });
+
+      await expectRevert(
+        markeplaceInstance.buyGradient(new BN(7), {
+          from: accounts[0]
+        }),
+        "Gradient can't be bought by owner"
+      );  
+    });
+  });
 });
