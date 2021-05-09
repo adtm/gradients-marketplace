abfa22d2de1cf3a23d6018dca8c8f0954fdb629c
Tomas Eglinskas <tomase@wix.com>
2021-05-10 01:22:28 +0300

    feat: be able to buy with approval to marketplace

--- src/Artist/Artist.tsx
+++ src/Artist/Artist.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom';
 import { shortenAddress } from '../utils/addressShortener';
-import Card from '../Collectible/OwnerCard';
+import Card from '../Collectible/Card';
+import OwnerCard from '../Collectible/OwnerCard';
 import Web3 from 'web3';
 import GradientTokenAbi from '../abi/GradientToken';
 import GradientMarketplaceAbi from '../abi/GradientMarketplace';
@@ -16,6 +17,9 @@ const Artist = () => {
   const TokenContractAddress = '0xDc3167542bfc1870418B21bDF73779193623dDE6'
   const MarketplaceContractAddress = '0x069Fe2C37333b2518f78C67DC3d8ED5b0b0E57BA'
   const ContractDeployerAddress = '0x79B2626a9DD5Cf8015eEd74409D76c6f0268dd24'
+
+  // get current address
+  const isOwner = (address == ContractDeployerAddress);
 
   const tokenContract = new web3.eth.Contract(GradientTokenAbi, TokenContractAddress, {
     from: ContractDeployerAddress,
@@ -68,7 +72,10 @@ const Artist = () => {
       <div className="container mx-auto">
         <div className="flex flex-wrap flex-auto justify-center">
           {gradients.map(gradient => (
-            <Card key={gradient.id} gradient={gradient} onCancelButton={cancelSale} onSellButton={setForSale} />
+            isOwner ? (<OwnerCard key={gradient.id} gradient={gradient} onCancelButton={cancelSale} onSellButton={setForSale} />) :
+              <Link to={`/gradient/${gradient.id}`}>
+                <Card gradient={gradient} />
+              </Link>
           ))}
         </div>
       </div>
--- src/Collectible/CollectibleScreen.tsx
+++ src/Collectible/CollectibleScreen.tsx
@@ -7,6 +7,7 @@ import { Link, useParams } from 'react-router-dom'
 import Web3 from 'web3';
 import GradientTokenAbi from '../abi/GradientToken';
 import GradientMarketplaceAbi from '../abi/GradientMarketplace';
+import BN from 'bn.js';
 
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
@@ -39,10 +40,24 @@ const CollectibleScreen = () => {
     setGradient(gradient);
   }
 
+  const buyGradient = async () => {
+    await tokenContract.methods.approve(MarketplaceContractAddress, id).send({
+      from: ContractDeployerAddress,
+    });
+
+    console.log("approved--")
+    await marketplaceContract.methods.buyGradient(id).send({
+      from: "0xb528BE1632B97d2cE23fd3C94493dB2d1072789E",
+      value: new BN(1000000),
+      gas: 1000000
+    });
+    getGradient();
+  }
+
   useEffect(() => {
     getGradient()
   }, [])
-  
+
   const DisabledBuyButton = () => {
     return <div className=" ">
       <h3 className="text-3xl font-semibold py-1">XXXX{" "}<span className="text-sm">ONE</span></h3>
@@ -57,11 +72,11 @@ const CollectibleScreen = () => {
     const servicePrice = price / 100 * 1.5
     return <div className="">
       <h3 className="text-3xl font-semibold py-1">{gradient.price.toLocaleString()} <span className="text-sm">ONE</span></h3>
-      <button className="md:w-auto w-full my-3 px-20 py-3 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700">
+      <button onClick={buyGradient} className="md:w-auto w-full my-3 px-20 py-3 font-semibold rounded-lg shadow-md text-white bg-black hover:bg-gray-700">
         Buy now
       </button>
       <p className="text-xs text-gray-500">Service fee <span className="text-gray-700 font-semibold">1.5%</span>, {servicePrice.toLocaleString()} ONE</p>
-      </div>
+    </div>
   }
 
 
@@ -71,31 +86,31 @@ const CollectibleScreen = () => {
     <div className="md:flex mt-40">
       <div className="flex flex-grow items-center justify-center">
         <div
-        className={`w-96 h-96 rounded-md`}
-        style={{ background: `linear-gradient(135deg, ${gradient.left} 0%, ${gradient.right} 100%)` }}
-      />
+          className={`w-96 h-96 rounded-md`}
+          style={{ background: `linear-gradient(135deg, ${gradient.left} 0%, ${gradient.right} 100%)` }}
+        />
       </div>
       <div className="md:flex-1">
 
         <div className="pb-2 pt-5">
           <h1 className="text-4xl font-bold">{gradient.left} - {gradient.right}</h1>
         </div>
- 
+
 
         <div className="pb-3">
           <div className="flex items-center">
             <h3 className="font-semibold">of{" "}
-            <Link className="text-blue-500" to={`/owner/${gradient.owner}`}>
+              <Link className="text-blue-500" to={`/owner/${gradient.owner}`}>
                 @{shortenAddress(gradient.owner)}
               </Link>
             </h3>
           </div>
-        </div> 
+        </div>
 
         <div className="pt-8 md:text-left text-center">
-          { gradient.forSale ? BuyButton(gradient.price) : DisabledBuyButton() }
+          {gradient.forSale ? BuyButton(gradient.price) : DisabledBuyButton()}
         </div>
-        
+
       </div>
     </div>
   )
