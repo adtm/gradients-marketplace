548911e22e9b7bc525390cf5826d389bb341bab6
Tomas Eglinskas <tomase@wix.com>
2021-05-16 13:40:00 +0300

    feat: add banner

---
+++ src/Banner/Banner.tsx
@@ -0,0 +1,48 @@
+import React, { useState, Fragment } from 'react'
+import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
+import { Transition } from '@headlessui/react';
+
+const Banner = () => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Transition
+      show={open}
+      as={Fragment}
+      enter="transition ease-out duration-100"
+      enterFrom="transform opacity-0"
+      enterTo="transform opacity-100"
+      leave="transition ease-in duration-75"
+      leaveFrom="transform opacity-100"
+      leaveTo="transform opacity-0"
+    >
+      <div className="bg-indigo-600">
+        <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between flex-wrap">
+            <div className="w-0 flex-1 flex items-center">
+              <span className="flex p-2 rounded-lg bg-indigo-800">
+                <SpeakerphoneIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </span>
+              <p className="ml-3 font-medium text-white truncate">
+                <span className="md:hidden">We announced a new product!</span>
+                <span className="hidden md:inline">Big news! We're excited to announce a brand NFT based on Harmony</span>
+              </p>
+            </div>
+            <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
+              <button
+                type="button"
+                onClick={() => setOpen(false)}
+                className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
+              >
+                <span className="sr-only">Dismiss</span>
+                <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  )
+}
+
+export default Banner;
--- src/Router.tsx
+++ src/Router.tsx
@@ -9,9 +9,11 @@ import Home from "./Screens/Home";
 import Artist from "./Screens/Owner";
 import Navigation from "./Navbar/Navbar";
 import GradientScreen from "./Screens/Gradient";
+import Banner from "./Banner/Banner";
 
 const Router = () => (
   <BrowserRouter>
+    <Banner />
     <Navigation />
     <div className="md:mx-10 md:my-10 sm:mx-10 sm:my-10 my-5 mx-5">
       <Routes>
